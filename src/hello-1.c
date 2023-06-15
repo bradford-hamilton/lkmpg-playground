f03ab85e389bf859fd57d764188c1d3eec00a851
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-06-15 15:15:26 -0600

    add simple kernel module and Makefile

---
+++ src/hello-1.c
@@ -0,0 +1,18 @@
+// The simplest kernel module
+
+#include <linux/module.h> // Needed by all modules
+#include <linux/printkh> // Needed for pr_info()
+
+int init_module(void)
+{
+  pr_info("Hello 1.\n");
+
+  return 0;
+}
+
+void cleanup_module(void)
+{
+  pr_info("Goodbye 1\n")
+}
+
+MODULE_LICENSE("GPL");
